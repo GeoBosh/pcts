879c53f33c1c1eb0e392f6541b27770a386c6ed9
Georgi Boshnakov <georgi.boshnakov@manchester.ac.uk>
2019-06-08 21:31:52 +0100

    add tests

--- tests/testthat/test-periodicTS.R
+++ tests/testthat/test-periodicTS.R
@@ -21,7 +21,7 @@ test_that("the new periodic classes are ok",
     expect_error(new("PeriodicTS_ts", z),
                  'not a scalar time series; consider "PeriodicMTS_ts"')
 
-    z.mts <- new("PeriodicMTS", z)
+    z.mts <- new("PeriodicMTS", z) ## TODO: something wrong here!
     z.mts <- new("PeriodicMTS_ts", z) # ok
     expect_identical(S3Part(z.mts, strictS3 = TRUE), z)
 
@@ -31,5 +31,32 @@ test_that("the new periodic classes are ok",
 
     as(AirPassengers, "PeriodicTS")
     as(AirPassengers, "PeriodicTS_ts")
+
+    ## as(AirPassengers, "PeriodicMTS")
+    as(AirPassengers, "PeriodicMTS_ts")
+    as(z, "PeriodicMTS_ts")
+
+    new("PeriodicTS_ts", 1:12, frequency = 4)
+    new("PeriodicTS_ts", matrix(1:12, ncol = 1), frequency = 4)
+    expect_error(new("PeriodicTS_ts", matrix(1:24, ncol = 2), frequency = 4),
+                 "not a scalar time series"
+
+    new("PeriodicMTS_ts", 1:12, frequency = 4)
+    new("PeriodicMTS_ts", matrix(1:12, ncol = 1), frequency = 4)
+
+    cycle(z.mts)
+    time(z.mts)
+
+    show(ap.ts)
+    show(ap.mts)
+
+    monthplot(ap.ts)
+    monthplot(ap.mts)
+
+    boxplot(ap.ts)
+    boxplot(ap.mts)
+
+    nTicks(ap.ts)
+
 
 })
